d14a506bdb0f651c18f3037ac8eb8c27d727a79d
dvsoo <qpzm2480@naver.com>
2018-10-11 19:46:39 +0900

    transaction fucking confusing ;-(

--- src/transaction.js
+++ src/transaction.js
@@ -1,4 +1,9 @@
-const CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js"),
+  utils = require("./utils"),
+  elliptic = require("elliptic");
+
+///initialize
+const ec = new elliptic.ec("secp256k1");
 
 class TxOut {
   constructor(address, amount) {
@@ -24,9 +29,9 @@ class Transaction {
 
 ////UTXO
 class UnxOut {
-  constructor(uTxOutId, uTxOutIndex, address, amount) {
-    this.uTxOutId = uTxOutId;
-    this.uTxOutIndex = uTxOutIndex;
+  constructor(txOutId, txOutIndex, address, amount) {
+    this.txOutId = txOutId;
+    this.txOutIndex = txOutIndex;
     this.address = address;
     this.amount = amount;
   }
@@ -49,4 +54,49 @@ const getTxId = tx => {
     .map(txOut => txOut.address + txOut.amount)
     .reduce((a, b) => a + b, "");
   return CryptoJS.SHA256(txInContent + txOutContent).toString();
+};
+
+////
+const findUTxOut = (txOutId, txOutIndex, uTxOutList) => {
+  return uTxOutList.find(
+    uTxOut => uTxOut.txOutId === txOutId && uTxOut.txOutIndex === txOutIndex
+  );
+};
+
+///Input 에 사인, 블록체인에게 유효하다고 검증해 주는 것.
+const signTxIn = (tx, txInIndex, privateKey, uTxOut) => {
+  ///우리가 원하는 input을 찾아야 된다.
+  const txIn = tx.txIns[txInIndex];
+  const dataToSign = tx.id;
+  //// 돈이 있는 지 체크한다.  To do: Find Tx 사용되지 않은 아웃풋
+  const referencedUTxOut = findUTxOut(txIn.txOutId, tx.txOutIndex, uTxOuts);
+  if (referencedUTxOut === null) {
+    return;
+  }
+
+  const key = ec.keyFromPrivate(privateKey, "hex");
+  const signature = utils.toHexString(key.sign(dataToSign).toDER());
+  return signature;
+};
+
+const updateUxOuts = (newTxs, uTxOutList) => {
+  ///트랜잭션응ㄴ 가끔 new transaction을 만든다.
+  const newUTxOuts = newTxs
+    .map(tx => {
+      tx.txOuts.map((txOut, index) => {
+        new UTxOut(tx.id, index, txOut.address, txOut.amount);
+      });
+    })
+    .reduce((a, b) => a.contact(b), []);
+
+  const spentTxOuts = newTxs
+    .map(tx => tx.txIns)
+    .reduce((a, b) => a.contact(b), [])
+    .map(txIn => new UTxOut(txIn.txOutId, txIn.txOutIndex, "", 0));
+
+  const resultingUTxOuts = uTxOutList
+    .filter(uTxO => !findUTxOut(uTxO.txOutId, uTxO.txOutIndex, spentTxOuts))
+    .concat(newUTxOuts);
+
+  return resultingUTxOuts;
 };
